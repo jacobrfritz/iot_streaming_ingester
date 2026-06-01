f16c686963d315218add13825da4740dd6738029
fritzcrackers <fritz.jacobr@gmail.com>
2026-05-31 23:22:12 -0500

    formatting

--- src/iot_streaming_ingester/main.py
+++ src/iot_streaming_ingester/main.py
@@ -2,12 +2,11 @@
 from iot_streaming_ingester.connector import RedisConnector
 from iot_streaming_ingester.worker import Worker
 
+
 async def run() -> None:
     conn = RedisConnector()
     await conn.create_group_if_not_exists()
-    
+
     worker = Worker(conn, 10)
     async for message in worker.run(10):
         print(message)
-    
-    
--- src/iot_streaming_ingester/worker.py
+++ src/iot_streaming_ingester/worker.py
@@ -8,14 +8,14 @@ from iot_streaming_ingester.interfaces import RedisMessage, RedisStreamResponse
 Processes stream chunks from orchestrator
 """
 
+
 class Worker:
-    def __init__(self, conn:Connector, max_message_count:int):
+    def __init__(self, conn: Connector, max_message_count: int):
         self.conn = conn
         self.max_message_count = max_message_count
-        self.worker_name:str = str(uuid.uuid4())
+        self.worker_name: str = str(uuid.uuid4())
 
-
-    def get_metrics(self, messages:RedisStreamResponse)-> tuple[int, float, float]:
+    def get_metrics(self, messages: RedisStreamResponse) -> tuple[int, float, float]:
         # Chan's Parallel Variance
         # returns count, mean, and running sum of squares for variance
         # needs to calculate for each producer
@@ -25,18 +25,18 @@ class Worker:
             delta = float()
             print(message)
         return 100, mean, ss
-    
-    async def run(self, max_message_count:int)->AsyncGenerator[list[RedisMessage], None]:
+
+    async def run(
+        self, max_message_count: int
+    ) -> AsyncGenerator[list[RedisMessage], None]:
         pending_messages = await self.conn.get_pending_messages(
-            worker_name = self.worker_name,
-            count = max_message_count
+            worker_name=self.worker_name, count=max_message_count
         )
         if pending_messages:
             self.get_metrics(pending_messages)
         while True:
             groups = await self.conn.read(
-                worker_name = self.worker_name,
-                count = self.max_message_count
+                worker_name=self.worker_name, count=self.max_message_count
             )
             # since there is only one group we are going to
             # assume we don't need to know the group
@@ -46,6 +46,3 @@ class Worker:
                     yield messages
                     #     metrics = self.get_metrics(messages)
                     #     yield metrics
-            
-            
-        
